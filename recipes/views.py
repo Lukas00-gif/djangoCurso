4e1d4e9f4e21005d962aa73f5f91009e3e27321d
Lukas00-gif <lucassky08@gmail.com>
2024-04-23 10:49:25 -0300

    alteration for header

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('Home 1')
-
-def contato(request):
-    return HttpResponse('Contato')
-
-def sobre(resquest):
-    return HttpResponse('sobre')
+    return render(request, 'recipes/pages/home.html')
