760fca1c35f04922b893815ccb8a8d1ae4275953
Lukas00-gif <lucassky08@gmail.com>
2024-04-26 16:15:46 -0300

    new template category and little changes

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
 
+from .models import Recipe
+
 # estou importando o factory o make_recipe que cria basicamente
 #varios nomes aleatorios para ajudar no preenchimento
 
 def home(request):
+    recipes = Recipe.objects.filter(
+        is_published=True,
+        ).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         #esse recipes aki de baixo recebi a lista aleatoria gerada
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
+    })
+
+def category(request, category_id):
+    #pedindo para filtrar atravez do category_id
+    # e so colocar o __ sendo q e uma forenkey = ao valor q eu quiser no caso category_id
+    recipes = Recipe.objects.filter(
+        category__id=category_id,
+        is_published=True,
+        ).order_by('-id')
+    return render(request, 'recipes/pages/category.html', context={
+        #esse recipes aki de baixo recebi a lista aleatoria gerada
+        'recipes': recipes,
     })
 
 def repice(request, id):
