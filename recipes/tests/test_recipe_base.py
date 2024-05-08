db0300ac7646da2b3b1b44d79f49214282fc3d01
Lukas00-gif <lucassky08@gmail.com>
2024-05-08 09:35:49 -0300

    more tests and reuse of test classes

---
+++ recipes/tests/test_recipe_base.py
@@ -0,0 +1,72 @@
+from django.test import TestCase
+from recipes.models import Category, Recipe, User
+
+#aqui e a base serve para serpar as classes e fazer reutilizar das mesmas
+# ou seja separando as classes dessa forma, vai herdar tudo os testes e quando for para
+# precisar fazer uma teste com fixture(dados para os testes) e so chamar essa funcoes
+#make_caategory, make_recipe ou make_author
+class RecipeTesteBase(TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+    
+    def make_category(self, name='Category'):
+        return Category.objects.create(name=name)
+    
+    def make_author(
+        self,
+        first_name='user',
+        last_name='name',
+        username='username',
+        password='123456',
+        email='username@gmail.com',
+    ):
+        return User.objects.create_user(
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            password=password,
+            email=email,
+        )
+    
+    def make_recipe(
+            self,
+            category_data = None,
+            author_data = None,    
+            title = 'Recipe Title',
+            description = 'Recipe Description',
+            slug = 'Recipe-slug',
+            preparation_time = '10',
+            preparation_time_unit = 'minutos',
+            servings = '5',
+            servings_unit = 'porçoes',
+            preparation_step = 'Recipe preparation step',
+            preparation_step_is_html = False,
+            is_published = True,
+            cover = 'teste'
+    ):
+        #vai verificar se o category data e None se for ele atribui um dicionario vazio
+        if category_data is None :
+            category_data = {}
+        
+        if author_data is None:
+            author_data = {}
+
+        return Recipe.objects.create(
+            #como e um dic vazio e so fazer desacompamento de dados **category_data
+            # que ele faz automatico
+            category = self.make_category(**category_data),
+            author = self.make_author(**author_data),    
+            title = title,
+            description = description,
+            slug = slug,
+            preparation_time = preparation_time,
+            preparation_time_unit = preparation_time_unit,
+            servings = servings,
+            servings_unit = servings_unit,
+            preparation_step = preparation_step,
+            preparation_step_is_html = preparation_step_is_html,
+            is_published = is_published,
+            cover = cover
+        )
+        
+        
--- recipes/tests/test_recipes_views.py
+++ recipes/tests/test_recipes_views.py
@@ -1,29 +1,75 @@
-from django.test import TestCase
 from django.urls import reverse, resolve
-
+from .test_recipe_base import RecipeTesteBase, Recipe
 
 from recipes import views
 
 
-
 #testar as views da aplicaçao
-class RecipeViewsTest(TestCase):
+#caso precisar de fazer um valor e so chamar o parametro correspondente
+#exemplo assim: self.make_recipe(preparation_time=5)
+class RecipeViewsTest(RecipeTesteBase):
     def test_recipe_home_view_function_is_correct(self):
         #detecta qual view esta, e o reverse pega a url para deixa-la aqui dinamica
         view = resolve(reverse('recipes:recipes-home'))
         #compara as duas views se sao iguais usando o is, ou seja se esta apontando para a
         #mesma locaçao de memoria
         self.assertIs(view.func, views.home)
+    
+    def test_recipe_home_view_returns_status_200_OK(self):
+        #ele vai pegar o get, da url e retornar o status
+        response = self.client.get(reverse('recipes:recipes-home'))
+        self.assertEqual(response.status_code, 200)
 
+    def test_recipe_home_view_loads_correct_template(self):
+        response = self.client.get(reverse('recipes:recipes-home'))
+        #passar esse asserttemplateused para fazer a compararaçao 
+        self.assertTemplateUsed(response, 'recipes/pages/home.html')
+    
+    def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
+        response = self.client.get(reverse('recipes:recipes-home'))
+        self.assertIn(
+            '<h1>No recipes found Here<h1>',
+            response.content.decode('utf-8')
+        )
+    
+    #teste com conteudo na home
+    def test_recipe_home_template_loads_recipes(self):
+        self.make_recipe()
+        response = self.client.get(reverse('recipes:recipes-home'))
+        content = response.content.decode('utf-8')
+        response_context_recipes = response.context['recipes']
+
+        self.assertIn('Recipe Title', content)
+        self.assertIn('10 minutos', content)
+        self.assertIn('5 porçoes', content)
+        self.assertEqual(len(response_context_recipes), 1)
+
+
+    #recipe
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(
-            reverse('recipes:category', kwargs={'category_id': 1})
+            reverse('recipes:category', kwargs={'category_id': 1000})
         )
         self.assertIs(view.func, views.category)
+    
+    def test_recipe_category_view_returns_404_if_no_recipes_found(self):
+        #ele vai pegar o get, da url e retornar o status
+        response = self.client.get(
+            reverse('recipes:category', kwargs={'category_id': 1000})
+        )
+        self.assertEqual(response.status_code, 404)
+    
 
+#detail
     def test_recipe_detail_view_function_is_correct(self):
         view = resolve(
             reverse('recipes:recipe', kwargs={'id': 1})
             )
         self.assertIs(view.func, views.repice)
-        
+    
+    def test_recipe_detail_view_returns_404_if_no_recipes_found(self):
+        #ele vai pegar o get, da url e retornar o status
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': 1000})
+        )
+        self.assertEqual(response.status_code, 404)
