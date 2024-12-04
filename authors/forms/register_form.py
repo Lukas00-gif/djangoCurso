60f4826f948944f06da61328cc2438437ec062aa
Lukas00-gif <lucassky08@gmail.com>
2024-12-04 09:53:53 -0300

    login system add

--- authors/forms/register_form.py
+++ authors/forms/register_form.py
@@ -1,30 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+from utils.django_forms import add_placeholder, strong_password
 
-import re
-
-
-def add_attr(field, attr_name, attr_new_val):
-    existing_attr = field.widget.attrs.get(attr_name, '')
-    field.widget.attrs[attr_name] = f'{existing_attr} {attr_new_val}'.strip()
-
-def add_placeholder(field, placeholder_val):
-    field.widget.attrs['placeholder'] = placeholder_val
-
-
-
-def strong_password(password):
-    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
-
-    if not regex.match(password):
-        raise ValidationError((
-            'Password must have at least one uppercase letter, '
-            'one lowercase letter and one number. The length should be '
-            'at least 8 characters.'
-        ),
-            code='invalid'
-        )
 
 
 class RegisterForm(forms.ModelForm):
--- authors/views.py
+++ authors/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
-from .forms import RegisterForm
 from django.http import Http404
 from django.contrib import messages
 from django.urls import reverse 
+from django.contrib.auth import authenticate, login 
 
+from .forms import RegisterForm, LoginForm
 
 
 def register_view(request):
@@ -12,7 +13,7 @@ def register_view(request):
         
     return render(request, 'authors/pages/resgister_view.html', {
         'form': form,
-        'form_action' : reverse('authors:create'),
+        'form_action' : reverse('authors:register_create'),
     })
 
 #vai ler e validar o que o register_View manda e depois manda novamente para o register_view
@@ -39,3 +40,39 @@ def register_create(request):
     # return render(request, 'authors/pages/resgister_view.html', {
     #     'form': form,
     # })
+
+def login_view(request):
+    form = LoginForm()
+    return render(request, 'authors/pages/login.html', {
+        'form': form,
+        'form_action': reverse('authors:login_create'),
+    })
+
+
+
+def login_create(request):
+    if not request.POST:
+        raise Http404()
+    
+    form  = LoginForm(request.POST)
+    login_url = reverse('authors:login')
+
+    if form.is_valid():
+        authenticated_user = authenticate(
+            username=form.cleaned_data.get('username', ''),
+            password=form.cleaned_data.get('password', ''),
+        )
+
+        #bem especifico
+        if authenticated_user is not None:
+            messages.success(request, 'You are logged in :)')
+            login(request, authenticated_user)
+        else:
+            messages.error(request, 'INVALID CREDENTIALS')
+    else:
+        messages.error(request, 'INVALID USERNAME OR PASSWORD')
+
+    return redirect(login_url)
+
+
+
