2c05a48de1065f17909f714a354efa083c88fcf3
Lukas00-gif <lucassky08@gmail.com>
2025-01-09 20:16:09 -0300

    correct dashboard is now displaying recipes

--- recipes/admin.py
+++ recipes/admin.py
@@ -8,7 +8,14 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'title', 'created_at', 'is_published')
+    list_display_links = ['title']
+    search_fields = ('id', 'title', 'description', 'slug', 'preparation_steps')
+    list_filter = ('category', 'author', 'is_published', 'preparation_step_is_html')
+    list_per_page = 10
+    list_editable = ['is_published']
+    ordering = ['-id']
+    prepopulated_fields = {"slug": ['title']}
 
 admin.site.register(Category, CategoryAdmin)
 
