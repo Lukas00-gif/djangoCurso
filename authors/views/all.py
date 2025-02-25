7f42f7353a7ae8b3be9dccd7bd511832a5b7cb3a
Lukas00-gif <lucassky08@gmail.com>
2025-02-25 09:46:47 -0300

    change FBV for CBV

--- authors/views/all.py
+++ authors/views/all.py
@@ -5,10 +5,9 @@ from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from authors.forms.recipe_form import AuthorRecipeForm
+from authors.forms import RegisterForm, LoginForm
 
 from recipes.models import Recipe
-
-from .forms import RegisterForm, LoginForm
 
 
 def register_view(request):
@@ -102,102 +101,6 @@ def dashboard(request):
     return render(request, 'authors/pages/dashboard.html',{
         'recipes' : recipes,
     })
-
-@login_required(login_url='authors:login', redirect_field_name='next')
-def dashboard_recipe_edit(request, id):
-    # ele retorna uma query set, entao deve ter o first no final pra nao dar erro
-    recipe = Recipe.objects.filter(
-        is_published=False,
-        author=request.user,
-        pk=id,
-    ).first()
-
-    if not recipe:
-        raise Http404()
-    
-    form = AuthorRecipeForm(
-        data=request.POST or None,
-        files=request.FILES or None,
-        instance=recipe
-    )
-
-    if form.is_valid():
-        recipe = form.save(commit=False)
-
-        #esse form e desse usuario
-        recipe.author = request.user
-        recipe.preparation_time_is_html = False
-        recipe.is_published = False
-
-        recipe.save()
-
-        messages.success(request, 'Your Recipe is save successfully  :)')
-        return redirect(reverse('authors:dashboard_recipe_edit', args=(id,)))
-
-
-    return render(request, 'authors/pages/dashboard_recipe.html',{
-        'form': form
-    })
-
-@login_required(login_url='authors:login', redirect_field_name='next')
-def dashboard_recipe_new(request):
-    form = AuthorRecipeForm(
-        data=request.POST or None,
-        files=request.FILES or None,
-    )
-
-    if form.is_valid():
-        recipe: Recipe = form.save(commit=False)
-
-        recipe.author = request.user
-        recipe.preparation_steps_is_html = False
-        recipe.is_published = False
-
-        recipe.save()
-
-        messages.success(request, 'Salvo com sucesso!')
-        return redirect(
-            reverse('authors:dashboard_recipe_edit', args=(recipe.id,))
-        )
-
-    return render(
-        request,
-        'authors/pages/dashboard_recipe.html',
-        context={
-            'form': form,
-            'form_action': reverse('authors:dashboard_recipe_new')
-        }
-    )
-
-
-@login_required(login_url='authors:login', redirect_field_name='next')
-def dashboard_recipe_delete(request):
-    if not request.POST:  
-        raise Http404()
-    
-    #ta salvando o dicionario do post inteiro ou seja os dados do form
-    POST = request.POST
-    id = POST.get('id')
-
-    # ele retorna uma query set, entao deve ter o first no final pra nao dar erro
-    recipe = Recipe.objects.filter(
-        is_published=False,
-        author=request.user,
-        pk=id,
-    ).first()
-
-    if not recipe:
-        raise Http404() 
-    
-    recipe.delete()
-    messages.success(request, 'Deleted successfully.')
-    return redirect(reverse('authors:dashboard'))
-
-
-
-
-
-
 
 
 
